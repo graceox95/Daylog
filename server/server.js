4b8d89222e7a9caccb954704ad9f3a42895ca1aa
JeongMan <graceox@naver.com>
2026-07-07 14:10:04 +0900

    기본적인 서버 구축 완료, 라우터 지정 완료

--- server/server.js
+++ server/server.js
@@ -23,16 +23,32 @@ const app = express();
 app.use("/images", express.static(path.join(__dirname, "../images")));
 app.use(express.static(path.join(__dirname, "../code")));
 
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "../code/home.html")); //절대경로 만드는 역할을 수행함.
-});
+app.get("/", function(req, res){
+    res.sendFile(path.join(__dirname, "../code/pages/home.html"))
+})
 
 app.get("/home", function (req, res) {
-    res.sendFile(path.join(__dirname, "../code/home.html")); //절대경로 만드는 역할을 수행함.
+    res.sendFile(path.join(__dirname, "../code/pages/home.html")); //절대경로 만드는 역할을 수행함.
 });
 
 app.get("/diary", function(req, res){
-    res.sendFile(path.join(__dirname, "../code/diary.html"));
+    res.sendFile(path.join(__dirname, "../code/pages/diary.html"));
+})
+
+app.get("/signUp", function(req, res){
+    res.sendFile(path.join(__dirname, "../code/auth/signUp.html"));
+})
+
+app.get("/signIn", function(req, res){
+    res.sendFile(path.join(__dirname, "../code/auth/signIn.html"))
+})
+
+app.get("/privacy", function(req, res){
+    res.sendFile(path.join(__dirname, "../code/auth/privacy.html"))
+})
+
+app.get("/terms", function(req, res){
+    res.sendFile(path.join(__dirname, "../code/auth/terms.html"))
 })
 
 app.listen(3000, function () {
